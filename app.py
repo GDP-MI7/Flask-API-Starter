b321c873b24fe3a40dcb9be5949d583a6b3a96c8
abhi <s530670@nwmissouri.edu>
2018-07-03 08:54:28 -0500

    Added get n post method for testing purpose

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
 
+
+@app.route("/test", methods=['GET', 'POST'])
+def user():
+    if request.method == 'GET':
+        return "Get request successful"
+    elif request.method == 'POST':
+        return "Post request successful"
+
 if __name__ == '__main__':
-   app.debug = True 
-   app.run()
+    app.debug = True
+    app.run()
